a382e75b600b6d613c2b025ddf766d978b9bb89e
Christopher Tate <computate@computate.org>
2022-04-11 23:06:16 -0600

    Working on the migrate data background task.

--- src/main/java/org/southerncoalition/enus/config/ConfigKeys.java
+++ src/main/java/org/southerncoalition/enus/config/ConfigKeys.java
@@ -7,4 +7,14 @@ import org.computate.vertx.config.ComputateVertxConfigKeys;
  * Description: To organize all the environment variables for the site in one place
  */
 public class ConfigKeys extends ComputateVertxConfigKeys {
+
+	/**
+	 * 
+	 **/
+	public static final String ENABLE_MIGRATE_DATA = "ENABLE_MIGRATE_DATA";
+
+	/**
+	 * 
+	 **/
+	public static final String MIGRATE_DATA_FETCH_SIZE = "MIGRATE_DATA_FETCH_SIZE";
 }
